0132b2c6a1dffaa7f079389984cb8d7e1e7b8300
Sharvan Raj Das <praj3399@gmail.com>
2022-03-23 23:03:08 +0530

    Day_2
    Various Pattern Problems.

---
+++ Pattern_for _Alex.py
@@ -0,0 +1,20 @@
+N = 10
+# for i in range(1,5):
+#     print(i*"*")
+
+# for i in range(5,0,-1):
+#     print(i*"*")
+#to be used
+# spaces = 5
+# numbers_stars = 1
+# for i in range(1,6):
+#     for j in range(i):
+#         print(" "*spaces,"*" * i)
+#     spaces -= 1
+#     numbers_stars += 1
+
+
+
+
+
+
